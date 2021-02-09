b5ccede78ceadb08940e82f98e4a79b382621481
AMIT <maityamit9800@gmail.com>
2021-02-09 09:38:57 +0530

    2nd commit

---
+++ app/src/main/java/modernexpensetrackerbyamitmaity/example/modernexpensetracker/FreindsCostActivity.java
@@ -0,0 +1,14 @@
+package modernexpensetrackerbyamitmaity.example.modernexpensetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FreindsCostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_freinds_cost);
+    }
+}
